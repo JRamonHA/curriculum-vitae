635c836a89d59ee695ff5de71c167fc4b24e756a
JRamonHA <jrha@ier.unam.mx>
2025-02-27 01:10:40 -0600

    Add podcast

--- assets/R/add_item_logo.R
+++ assets/R/add_item_logo.R
@@ -1,51 +1,49 @@
-
 add_item_logo <- function(url, type, colour = "#333333") {
   
-  
-  
   if(type == "github") {
-  string <- sub(
-    "[GitHub]", #pattern
-    paste0("[", fontawesome::fa("github", fill = colour), " GitHub repository]"), #replacement
-    # x (string)
-    sub(
-      pattern = "(.*)https://github.com/(.*)",
-      replacement = "\\1[GitHub](https://github.com/\\2)",
-      x = url
+    string <- sub(
+      "[GitHub]", # pattern
+      paste0("[", fontawesome::fa("github", fill = colour), " GitHub repository]"), # replacement
+      sub(
+        pattern = "(.*)https://github.com/(.*)",
+        replacement = "\\1[GitHub](https://github.com/\\2)",
+        x = url
+      ),
+      fixed = TRUE
     )
-    ,
-    fixed = TRUE
-  )
   }
   
   if(type == "presentation") {
-  string <-  sub(
-      "[Presentation]", #pattern
-      paste0("[", fontawesome::fa("file-powerpoint", fill = colour), " Slides]"), #replacement
-    sub(
-      pattern = "(.*)https://blenback.github.io/presentations/(.*)",
-      replacement = "\\1[Presentation](https://blenback.github.io/presentations/\\2)",
-      x = url
+    string <- sub(
+      "[Presentation]", # pattern
+      paste0("[", fontawesome::fa("file-powerpoint", fill = colour), " Slides]"), # replacement
+      sub(
+        pattern = "(.*)https://blenback.github.io/presentations/(.*)",
+        replacement = "\\1[Presentation](https://blenback.github.io/presentations/\\2)",
+        x = url
+      ),
+      fixed = TRUE
     )
-    ,
-    fixed = TRUE
-  )
+  }
+  
+  if(type == "spotify") {
+    # Crear el enlace Markdown directamente con el logo y "Podcast"
+    string <- paste0("[", fontawesome::fa("spotify", fill = colour), " Podcast](", url, ")")
   }
   
   if(type == "website") {
-  string <-  sub(
-      "[Website]", #pattern
+    string <- sub(
+      "[Website]", # pattern
       paste0("[", fontawesome::fa("globe", fill = colour), " Website]"),
-        sub(
-        pattern = paste0("(.*)", url, "(.*)"),
-        replacement = paste0("\\1[Website](", url, "\\2)"),
+      sub(
+        pattern = "(.*)", # Captura cualquier texto antes del URL
+        replacement = "\\1[Website](", # Reemplaza con el texto seguido de [Website](
         x = url
+      ),
+      fixed = TRUE
     )
-    ,
-    fixed = TRUE
-    )
+    string <- paste0(string, url, ")") # Añade el URL y el cierre del enlace
   }
   
   return(string)
 }
-  
