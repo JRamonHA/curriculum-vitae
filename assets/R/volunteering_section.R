7480a50a2a685f33ee1d666baeb6bdf306c46c64
JRamonHA <jrha@ier.unam.mx>
2025-04-13 16:44:57 -0600

    Added volunteering section

---
+++ assets/R/volunteering_section.R
@@ -0,0 +1,45 @@
+
+volunteering_section <- function(xlsx = "data/cv.xlsx", sheet = "volunteering", page_break_after = FALSE) {
+  
+  #OPTION TO SPLIT ACTIVITIES INTO BULLET POINT LIST
+  # #read text from xlsx
+  # text <- read_excel_sheet(xlsx, sheet)
+  # 
+  # #Put the unicode character 	"\u2022" in front of each test$activities entry and 
+  # text$activities <- sapply(text$activities, function(x) {
+  #   
+  #   open_bullet <- paste0("\u2022 ", x)
+  # 
+  #   #replace instances of '.' with the unicode character 	"\n\u2022" except the last instance
+  #   open_bullet <- gsub("\\.", ". \n\n\u2022", open_bullet)
+  #   
+  #   #remove the last instance in the string of "\n\u2022"
+  #   open_bullet <- stringi::stri_replace_last(open_bullet,
+  #                                             replacement = "\\.",
+  #                                             regex = ". \n\n\u2022")
+  #   })
+  # 
+  # #reformat output with sprintf
+  # text <- text[
+  #   i = .N:1,
+  #   j = sprintf(
+  #     "### %s\n\n%s\n\n%s\n\n%s - %s\n\nActivities: \n\n*%s*\n\n\n\n",
+  #     position, institute, city, start, end, activities
+  #   )
+  # ]
+  
+  text <- read_excel_sheet(xlsx, sheet)[
+    i = .N:1,
+    j = sprintf(
+      "### %s\n\n%s\n\n%s\n\n%s - %s\n\nActivities: *%s*\n\n\n\n",
+      position, institute, city, start, end, activities
+    )
+  ]
+  
+
+  if (page_break_after) {
+    c("## Volunteering {data-icon=handshake-angle .break-after-me}", text)
+  } else {
+    c("## Volunteering {data-icon=handshake-angle}", text)
+  }
+}
