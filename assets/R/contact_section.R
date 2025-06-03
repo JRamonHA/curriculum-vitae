648a8863d116caa2098c5ce1d9d91e61803e135c
JRamonHA <jrha@ier.unam.mx>
2025-06-03 01:09:07 -0600

    website added

--- assets/R/contact_section.R
+++ assets/R/contact_section.R
@@ -8,7 +8,7 @@ contact_section <- function(xlsx = "data/cv.xlsx", sheet = "contact", colour = "
         "- %s %s",
         "- %s [%s](mailto:%s)",
         "- %s %s", # phone,
-        #"- %s [%s](%s)",
+        "- %s [%s](%s)",
         #"- %s [%s](https://orcid.org/%s)",
         "- %s [%s](https://www.linkedin.com/in/%s)",
         "- %s [%s](https://github.com/%s)",
@@ -22,7 +22,7 @@ contact_section <- function(xlsx = "data/cv.xlsx", sheet = "contact", colour = "
       fontawesome::fa("map-location-dot", fill = colour), city,
       fontawesome::fa("envelope", fill = colour), gsub("\\.", "\\.", sub("@", "@", email)), email,
       fontawesome::fa("phone", fill = colour), phone,
-      #fontawesome::fa("house", fill = colour), sub("/$", "", sub("https*://", "", website)), website,
+      fontawesome::fa("house", fill = colour), sub("/$", "", sub("https*://", "", website)), website,
       #fontawesome::fa("orcid", fill = colour), orcid, orcid,
       fontawesome::fa("linkedin", fill = colour), linkedin, linkedin,
       fontawesome::fa("github", fill = colour), github, github
